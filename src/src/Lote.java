f461891c3cc98b2ac031591c2e485cc2544a478a
biancacpacheco <bianca.pacheco@ccc.ufcg.edu.br>
2022-05-09 10:01:08 -0300

    classe lote e produto

---
+++ src/src/Lote.java
@@ -0,0 +1,27 @@
+public class Lote {
+    private String id;
+    private Product product;
+    private int quantity;
+
+    public Lote(String id, Product produto, int quantidade) {
+        this.id = id;
+        this.product = product;
+        this.quantity = quantidade;
+    }
+
+    public String toString(){
+        return(product + ": " + id + "- " + quantity + "unidade(s) disponíveis");
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public Product getProduct() {
+        return this.product;
+    }
+
+    public int getQuantitiy() {
+        return this.quantity;
+    }
+}
---
+++ src/src/Product.java
@@ -0,0 +1,30 @@
+public class Product {
+    private String id;
+    private String name;
+    private String fabricante;
+
+    public Product(String nome, String fabricante, String id) {
+        this.id = id;
+        this.name = nome;
+        this.fabricante = fabricante;
+    }
+
+    public String toString(){
+        return(name + ", fabricado por: " + fabricante + ", identificação: " + id );
+    }
+
+    public String getId() {
+
+        return this.id;
+    }
+
+    public String getName() {
+
+        return this.name;
+    }
+
+    public String getFabricante() {
+
+        return this.fabricante;
+    }
+}
